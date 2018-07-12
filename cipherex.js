12b944df83e4b97f55e9fe2d15bba1f460fdf0e0
Ryan Carson <rcarson95@gmail.com>
2018-07-12 10:26:40 -0400

    Encryption finished

---
+++ cipherex.js
@@ -0,0 +1,32 @@
+const encrypt = (key, input) => {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const extendedKey = extendKey(key, input);
+    const encrypted = [];
+    for (let i = 0; i < input.length; i++) {
+        encrypted.push(encryptLetter(alphabet, input.substr(i, 1), extendedKey.substr(i, 1)));
+    }
+    return encrypted.join("");
+};  
+
+const extendKey = (key, input) => {
+    let extendedKey = key;
+    for (let i = 0; i < Math.floor(input.length / key.length) - 1; i++) {
+        extendedKey += key;
+    }
+    extendedKey += key.slice(0, input.length % key.length);
+    return extendedKey;
+};
+
+const shiftAlphabet = (alphabet, index) => {
+    return alphabet.slice(index) + alphabet.slice(0, index);
+};
+
+const encryptLetter = (alphabet, inputLetter, keyLetter) => {
+    const shiftedAlphabet = shiftAlphabet(alphabet, alphabet.search(keyLetter));
+    return shiftedAlphabet.substr(alphabet.search(inputLetter), 1);
+};
+
+console.log(encrypt("snitch", "thepackagehasbeendelivered"));
+console.log(encrypt("bond", "theredfoxtrotsquietlyatmidnight"));
+console.log(encrypt("train", "murderontheorientexpress"));
+console.log(encrypt("garden", "themolessnuckintothegardenlastnight"));
